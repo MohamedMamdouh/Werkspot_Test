6b83ec8064b7f6aafc28cef374384afcf76eca47
Mohamed Mamdouh <eng_mohamed93@msn.com>
2018-06-16 12:39:59 +0200

    - add toDos for HackerNews Item

--- App/Components/HackerNewsListItem.js
+++ App/Components/HackerNewsListItem.js
@@ -1,36 +1,40 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { View, Text } from "react-native";
-import styles from "./Styles/HackerNewsListItemStyles";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { View, Text } from 'react-native'
+import styles from './Styles/HackerNewsListItemStyles'
 import moment from 'moment'
 
+// ToDo: 
+// Adding proptypes
+// Polish UI 
+// Make it clickable to navigate to story link
 
 export default class HackerNewsListItem extends Component {
-  // static defaultProps = { show: true };
 
-  //   static propTypes = {
-  //     title: PropTypes.string,
-  //     icon: PropTypes.string,
-  //     style: PropTypes.object,
-  //     show: PropTypes.bool
-  //   }
-
-  render() {
+  render () {
     const {url, title, comments, time, author} = this.props
     return (
       <View style={styles.container}>
         <View style={styles.titleView}>
-          <Text style={styles.titleText}>{title}</Text>
-          <Text style={styles.commentsNumber}>{comments}</Text>
+          <Text style={styles.titleText}>
+            {title}
+          </Text>
+          <Text style={styles.commentsNumber}>
+            {comments}
+          </Text>
         </View>
         <View style={styles.autherView}>
-          <Text style={styles.autherText}>{author}</Text>
-          <Text style={styles.timeText}>{(moment().diff(moment(time), 'days') < 7) ? moment(time).fromNow() : moment(time).format('MMMM Do YYYY')}</Text>
+          <Text style={styles.autherText}>
+            {author}
+          </Text>
+          <Text style={styles.timeText}>
+            {;(moment().diff(moment(time), 'days') < 7) ? moment(time).fromNow() : moment(time).format('MMMM Do YYYY')}
+          </Text>
         </View>
         <Text style={styles.linkText}>
           {url && url.substring(0, 45)}...
         </Text>
       </View>
-    );
+    )
   }
 }
